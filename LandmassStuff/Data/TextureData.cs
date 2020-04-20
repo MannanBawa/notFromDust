b013889e12dea9f7455e61e9af727b5ec5158440
Mannan Bawa <bawa.m@husky.neu.edu>
2020-04-20 14:18:47 -0400

    Implement custom Color shader for different heights

--- LandmassStuff/Data/TextureData.cs
+++ LandmassStuff/Data/TextureData.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class TextureData : UpdateableData {
 
+    public Color[] baseColors;
+    [Range(0,1)]
+    public float[] baseStartHeights;
+
+    // public float[] 
+
+    float savedMinHeight;
+    float savedMaxHeight;
+
     public void ApplyToMaterial (Material material) {
-        // Do stuff
+        material.SetInt("baseColorCount", baseColors.Length);
+        material.SetColorArray("baseColors", baseColors);
+        material.SetFloatArray("baseStartHeights", baseStartHeights);
+
+        UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
+    }
+
+    public void UpdateMeshHeights(Material material, float minHeight, float maxHeight) {
+        savedMinHeight = minHeight;
+        savedMaxHeight = maxHeight;
+
+        material.SetFloat("minHeight", minHeight);
+        material.SetFloat("maxHeight", maxHeight);
     }
 
 }
